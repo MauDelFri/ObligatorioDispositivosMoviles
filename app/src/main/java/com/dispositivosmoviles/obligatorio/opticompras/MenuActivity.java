532a97a965fe620abf53e0f27ad11b776334361e
Mauricio <maunintendo@gmail.com>
2015-11-11 21:04:04 -0300

    Diseño login y menu principal

---
+++ app/src/main/java/com/dispositivosmoviles/obligatorio/opticompras/MenuActivity.java
@@ -0,0 +1,17 @@
+package com.dispositivosmoviles.obligatorio.opticompras;
+
+import android.graphics.PorterDuff;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+    }
+}
