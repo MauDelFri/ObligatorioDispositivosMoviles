0aaec3d93459a590a4c0975019af90f997bb2be9
Chrisman <chrismang@hotmail.es>
2015-11-27 08:34:08 -0300

    Sube codigo

--- app/src/main/java/com/dispositivosmoviles/obligatorio/opticompras/MenuActivity.java
+++ app/src/main/java/com/dispositivosmoviles/obligatorio/opticompras/MenuActivity.java
@@ -1,10 +1,12 @@
 package com.dispositivosmoviles.obligatorio.opticompras;
 
+import android.content.Intent;
 import android.graphics.PorterDuff;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -12,6 +14,10 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+    }
 
+    public void verMapa(View v) {
+        Intent intent = new Intent(v.getContext(), MapaActivity.class);
+        startActivity(intent);
     }
 }
